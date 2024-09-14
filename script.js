a6aedbc4fea1982d636046767184c5d3874f59ce
darkfusion390 <64776111+darkfusion390@users.noreply.github.com>
2024-09-14 11:47:24 -0500

    testing

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ function checkSchedule() {
         console.log(period.label); // Log the period label to the console
         document.getElementById('periodLabel').innerText = period.label;
     } else {
+		playBell();
         document.getElementById('periodLabel').innerText = "Waiting for schedule...";
     }
 }
